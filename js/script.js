51f1a45d83f6c23eff51154ac70bf3e64bca7ddd
skoms <45171368+skoms@users.noreply.github.com>
2020-12-14 16:52:57 +0100

    Adding display functionality

--- js/script.js
+++ js/script.js
@@ -6,11 +6,42 @@ const designsSelect = document.getElementById('design');
 const jsPunsColors = document.querySelectorAll("[data-theme='js puns']");
 const heartJsColors = document.querySelectorAll("[data-theme='heart js']");
 const activityFieldset = document.getElementById('activities');
+let totalCostDisplay = document.getElementById('activities-cost');
+let totalCost = 0;
+const paymentsSelect = document.getElementById('payment');
+const creditCardDiv = document.getElementById('credit-card');
+const paypalDiv = document.getElementById('paypal');
+const bitcoinDiv = document.getElementById('bitcoin');
+
+const changePayment = value => {
+    const method = value;
+    switch(method) {
+        case 'credit-card':
+            paypalDiv.style.display = 'none';
+            bitcoinDiv.style.display = 'none';
+            creditCardDiv.style.display = 'block';
+            break;
+        case 'paypal':
+            paypalDiv.style.display = 'block';
+            bitcoinDiv.style.display = 'none';
+            creditCardDiv.style.display = 'none';
+            break;
+        case 'bitcoin':
+            paypalDiv.style.display = 'none';
+            bitcoinDiv.style.display = 'block';
+            creditCardDiv.style.display = 'none';
+            break;
+    }
+}
 
 const initializePage = () => {
     nameField.focus();
     otherJobRoleField.style.display = 'none';
     colorsSelect.style.display = 'none';
+    paymentsSelect[1].selected = true;
+    paypalDiv.style.display = 'none';
+    bitcoinDiv.style.display = 'none';
+    creditCardDiv.style.display = 'block';
 }
 
 jobRoles.addEventListener('change', e => {
@@ -41,7 +72,17 @@ designsSelect.addEventListener('change', e => {
 });
 
 activityFieldset.addEventListener('change', e => {
-    
+    const activity = e.target;
+    if( activity.checked ) {
+        totalCost += +activity.dataset.cost;
+    } else if( totalCost > 0) {
+        totalCost -= +activity.dataset.cost;
+    }
+    totalCostDisplay.innerHTML = `Total: $${totalCost}`;
+});
+
+paymentsSelect.addEventListener('change', e => {
+    changePayment( e.target.value );
 });
 
 // Initializing the page
